058f9847a1c27a339cfbdf03db8fbe35347ec144
Lowkey Ud <122513007+vexify1337@users.noreply.github.com>
2025-01-23 03:22:08 +0000

    Create visible check.h

---
+++ visible check.h
@@ -0,0 +1,6 @@
+bool IsEnemyVisible(uintptr_t mesh)
+{
+	auto Seconds = Kernel.Read<double>(CachePointers.UWorld + 0x148);
+	auto LastRenderTime = Kernel.Read<float>(mesh + 0x30C);
+	return Seconds - LastRenderTime <= 0.06f;
+}
